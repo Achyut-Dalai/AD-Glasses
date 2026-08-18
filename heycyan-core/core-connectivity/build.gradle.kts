0a3b34396ab4fa94f22f63b307dd3ba78d90228d
Achyut-Dalai <79808239+Achyut-Dalai@users.noreply.github.com>
2026-08-18 18:34:11 +0530

    build: modernize core connectivity

--- heycyan-core/core-connectivity/build.gradle.kts
+++ heycyan-core/core-connectivity/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 
 android {
     namespace = "com.heycyan.core.connectivity"
-    compileSdk = 35
+    compileSdk = 37
 
     defaultConfig {
         minSdk = 24
@@ -22,8 +22,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     publishing {
@@ -34,7 +34,7 @@ android {
 }
 
 dependencies {
-    api("androidx.core:core:1.16.0")
+    api("androidx.core:core:1.19.0")
     api("androidx.arch.core:core-common:2.2.0")
     testImplementation("junit:junit:4.13.2")
 }
