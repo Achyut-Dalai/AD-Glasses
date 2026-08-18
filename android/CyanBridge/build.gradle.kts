2ab01939b7ba9cd1513c8ae41d9ef136ebff3efa
Achyut-Dalai <79808239+Achyut-Dalai@users.noreply.github.com>
2026-08-18 18:21:16 +0530

    build: adopt AGP 9 built-in Kotlin

--- android/CyanBridge/build.gradle.kts
+++ android/CyanBridge/build.gradle.kts
@@ -4,12 +4,17 @@ buildscript {
         google()
         mavenCentral()
     }
+    dependencies {
+        // AGP 9 has built-in Kotlin. Pin the current stable KGP explicitly so
+        // Android and multiplatform modules compile with the same Kotlin toolchain.
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.10")
+    }
 }
 
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.android.kotlin.multiplatform.library) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.compose.multiplatform) apply false
     alias(libs.plugins.kotlin.compose.compiler) apply false
