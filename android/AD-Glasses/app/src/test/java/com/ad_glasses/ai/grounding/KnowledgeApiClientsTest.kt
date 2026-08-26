96e7bcb46139679352bee6630a027021b7d3f3ae
Achyut-Dalai <79808239+Achyut-Dalai@users.noreply.github.com>
2026-08-27 01:13:49 +0530

    Run structured knowledge JSON tests under Robolectric

--- android/AD-Glasses/app/src/test/java/com/ad_glasses/ai/grounding/KnowledgeApiClientsTest.kt
+++ android/AD-Glasses/app/src/test/java/com/ad_glasses/ai/grounding/KnowledgeApiClientsTest.kt
@@ -2,7 +2,12 @@ package com.ad_glasses.ai.grounding
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [34])
 class KnowledgeApiClientsTest {
     private val currency = FrankfurterCurrencyClient()
 
